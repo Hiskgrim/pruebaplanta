768afa086ea1c90e846568800f4e7f5fa239099f
Mauricio Rincón <causemao@gmail.com>
2017-08-09 12:04:07 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/astaxie/beego/plugins/cors"
 )
 
+func init() {
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGhost")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
+}
+
 func main() {
 	orm.Debug = true
 	if beego.BConfig.RunMode == "dev" {
